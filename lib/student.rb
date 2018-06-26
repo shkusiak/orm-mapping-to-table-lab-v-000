e108ae271c4f34f32b5fbb6a1cc259f3ea65e831
Shea Kusiak <shkusiak@gmail.com>
2018-06-26 00:58:13 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,8 +6,8 @@ class Student
     @grade = grade
     @name = name
   end
-  
+
   # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
-  
+  #  with DB[:conn]
+
 end
